880b5a8fb169e5b81cd86965b32e0193b7046c03
fanglang <1370302653@qq.com>
2022-12-26 10:07:50 +0800

    springmvc_rest and springmvc_fileUpAndDown

---
+++ ssm/springmvc_rest/src/main/java/road/controller/EmployeeController.java
@@ -0,0 +1,90 @@
+package road.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import road.dao.EmployeeDao;
+import road.pojo.Employee;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+
+/**
+ * 查询所有的员工信息-->/employee-->get
+ * 跳转到添加页面-->/to/add-->get
+ * 新增员工信息-->/employee-->post
+ * 跳转到修改页面-->/employee/1-->get
+ * 修改员工信息-->/employee-->put
+ * 删除员工信息-->/employee/1-->delete
+ */
+
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+
+    /**
+     * 查看所有员工
+     * @return
+     */
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getAllEmployees(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("employees", employees);
+        return "employee_list";
+    }
+
+    /**
+     * 新增员工信息
+     * @param employee
+     * @return
+     */
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    /**
+     * to update
+     */
+    @RequestMapping( value = "/employee/{id}", method = RequestMethod.GET)
+    public String toUpdateEmp(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id); //查出要修改员工的信息
+        model.addAttribute("employee", employee); //把信息放到请求域中
+        return "employee_update"; //跳转到修改页面
+    }
+
+    /**
+     * 修改员工信息
+     * @param employee
+     * @return
+     */
+    @RequestMapping(value = "/employee", method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+
+    /**
+     * 删除员工信息
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee"; //这个没有写
+    }
+
+
+
+}
---
+++ ssm/springmvc_rest/src/main/java/road/controller/ManagerJSONController.java
@@ -0,0 +1,80 @@
+package road.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import road.pojo.User;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * springmvc 使用注解处理JSON
+ * @RequestBody 处理json格式的请求参数，将请求体中的内容和控制器方法的形参进行绑定
+ * @ResponseBody 响应json格式的数据，将所标识的控制器方法的返回值作为响应报文的响应体响应到浏览器
+ * @RestController 相当于 @Controller + @ResponseBody
+ */
+
+@Controller
+//@RestController   = @Controller + @ResponseBody
+public class ManagerJSONController {
+
+    @RequestMapping("/test/ajax")
+    public void testAjax(Integer id, @RequestBody String requestBody, HttpServletResponse response) throws IOException {
+        System.out.println("requestBody:"+requestBody);
+        System.out.println("id:"+id);
+        response.getWriter().write("hello,axios");
+    }
+
+    @RequestMapping("/test/RequestBody/json")
+    public void testRequestBody(@RequestBody Map<String, Object> map, HttpServletResponse response) throws IOException {
+        System.out.println(map);
+        response.getWriter().write("hello,RequestBody");
+    }
+
+    public void testRequestBody(@RequestBody User user, HttpServletResponse response) throws IOException {
+        System.out.println(user);
+        response.getWriter().write("hello,RequestBody");
+    }
+
+    @RequestMapping("/test/ResponseBody")
+    @ResponseBody
+    public String testResponseBody(){
+        return "success";
+    }
+
+    @RequestMapping("/test/ResponseBody/json")
+    @ResponseBody
+    public List<User> testResponseBodyJson(){
+        User user1 = new User(1001, "admin1", "123456", 20, "男");
+        User user2 = new User(1002, "admin2", "123456", 20, "男");
+        User user3 = new User(1003, "admin3", "123456", 20, "男");
+        List<User> list = Arrays.asList(user1, user2, user3);
+        return list;
+    }
+
+    /*public Map<String, Object> testResponseBodyJson(){
+        User user1 = new User(1001, "admin1", "123456", 20, "男");
+        User user2 = new User(1002, "admin2", "123456", 20, "男");
+        User user3 = new User(1003, "admin3", "123456", 20, "男");
+        Map<String, Object> map = new HashMap<>();
+        map.put("1001", user1);
+        map.put("1002", user2);
+        map.put("1003", user3);
+        return map;
+    }*/
+    /*public User testResponseBodyJson(){
+        User user = new User(1001, "admin", "123456", 20, "男");
+        return user;
+    }*/
+
+
+
+
+}
